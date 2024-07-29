f2723cbfb9c8eae0ccd7a113a2d6e98d6d5ab144
Sol Thiessen <soljthiessen@gmail.com>
2024-07-29 14:14:35 +0200

    bruh

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 import torch
 
-DEVICE = torch.device("mps") if torch.device("mps") else torch.device("cpu")
+DEVICE = torch.device("cuda") if torch.device("cuda") else torch.device("cpu")
 N_u, N_v = (10000, 1000)
 
 TRAIN_PATH = "../data/raw_data/train.csv"
